0ec7735f7e52fe74a2e65d7dfcceb8980ca72a7b
liuzzh1971 <Liuzzh1971@163.com>
2026-06-29 15:08:15 +0800

    Add files via upload

---
+++ 文件名 moyu4k.js
@@ -0,0 +1,9 @@
+// 文件名: moyu4k.js
+function init() { log('【摸鱼4K】JS规则已加载'); }
+function home(filter) { return JSON.stringify({ class: [] }); }
+function home（filter） { return JSON.stringify（{ 类： [] }）; }
+function category(tid, pg, filter, extend) { return ''; }
+函数类别（tid， pg， filter， extend） { return ''; }
+function detail(id) { return ''; }
+function detail（id） { return ''; }
+function play(flag, id, flags) { return id; }
